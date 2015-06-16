a86e384223609434849057dfb5cb775a7b682f4c
olley102 <olley102@gmail.com>
2015-06-16 17:59:15 +0100

    Unity 5.1 Rebuild
    New scripts have been made as well as the FallScript.cs being edited.
    Plus, the project has been rebuilt due to an update to Unity 5.1.

---
+++ Assets/Scripts/CollidedScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollidedScript : MonoBehaviour {
+	public int collided = 0;
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Player" || col.gameObject.name == "Ball") {
+			collided = 1;
+		}
+	}
+}
